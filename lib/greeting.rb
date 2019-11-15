8f682d7266344e6bf13e8b0130f0aa2c329d577a
Adriene Matsudaira <adriene.matsudaira@wework.com>
2019-11-15 06:13:26 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -3,10 +3,3 @@ def greeting(name)
   puts "Hello, #{name}. It's nice to meet you."
 end
 
-def greeting(name)
-  puts "Hello #{name}. It's nice to meet you."
-end
-
-def greeting(name)
-  puts "Hello, #{name}! It's nice to meet you."
-end
